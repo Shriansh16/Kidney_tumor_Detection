56cae1157d924da1ac03c43768948109673f5f7e
Shriansh16 <shrianshsingh20@gmail.com>
2023-11-26 19:39:04 +0530

    first commit

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ sys.path.insert(0, 'D:\Kidney_tumor_detection\src')
 from logger import logging
 from exception import CustomException
 from src.pipelines.stage_01_data_ingestion import DataIngestionPipeline
+from src.pipelines.stage_02_prepare_base_model import PrepareBaseModelPipeline
 
 STAGE_NAME='DATA INGESTION STAGE'
+STAGE_NAME2='PREPARE BASE MODEL STAGE'
 try:
-        logging.info('{STAGE_NAME} HAS STARTED')
+        logging.info(f'{STAGE_NAME} HAS STARTED')
         obj=DataIngestionPipeline()
         obj.main()
-        logging.info('{STAGE_NAME} HAS COMPLETED')
+        logging.info(f'{STAGE_NAME} HAS COMPLETED')
 
 except Exception as e:
         logging.info('ERROR OCCURED IN DATA INGESTION PIPELINE')
+        raise CustomException(e,sys)
+
+
+try:
+        obj2=PrepareBaseModelPipeline()
+        logging.info(f'{STAGE_NAME2} HAS STARTED')
+        obj2.main()
+        logging.info(f'{STAGE_NAME2} HAS completed')
+except Exception as e:
+        logging.info("error occured in prepare base model pipeline")
         raise CustomException(e,sys)
---
+++ src/pipelines/stage_02_prepare_base_model.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.insert(0, 'D:\Kidney_tumor_detection\src')
+from config.configuration import *
+from components.prepare_base_model import *
+from logger import *
+from exception import *
+
+STAGE_NAME='PREPARE BASE MODEL'
+
+class PrepareBaseModelPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        try:
+            configuration_manager=ConfigurationManager(CONFIG_FILE_PATH,PARAM_FILE_PATH)
+            base_model_config=configuration_manager.get_prepare_base_model_config()
+            prepare_base_model=PrepareBaseModel(base_model_config)
+            prepare_base_model.get_base_model()
+            prepare_base_model.update_base_model()
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=='__main__':
+    try:
+        obj2=PrepareBaseModelPipeline()
+        logging.info(f'{STAGE_NAME} HAS STARTED')
+        obj2.main()
+        logging.info(f'{STAGE_NAME} HAS completed')
+    except Exception as e:
+        logging.info("error occured in prepare base model pipeline")
+        raise CustomException(e,sys)
+
+        
